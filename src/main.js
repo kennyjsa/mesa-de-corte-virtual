30b2b3681ba65e91dada0df96031d42514b5739c
Kenny Johnson <kenny.barao@gmail.com>
2019-03-08 17:03:54 -0400

    teste

--- src/main.js
+++ src/main.js
@@ -11,13 +11,23 @@ navigator.mediaDevices.enumerateDevices()
   .then(function (devices) {
     var texto = '';
     devices.forEach(function (device) {
-      texto += (device.kind + ": " + device.label +
-        " id = " + device.deviceId);
+      texto += '<b class="btn" onclick="setDevice("'+ device.deviceId +'")">' + (device.kind + ": " + device.label + " id = " + device.deviceId) + '</b>';
+
         texto += '\n';
     });
 
-    document.getElementById('texto').innerText = texto;
+    document.getElementById('texto').innerHTML = texto;
   })
   .catch(function (err) {
     document.getElementById('texto').innerText = (err.name + ": " + err.message);
   });
+
+
+  function setDevice(myPreferredCameraDeviceId){
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: { deviceId: myPreferredCameraDeviceId } }).then(function (stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      });
+    }
+  }
